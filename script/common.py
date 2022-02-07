d46ca1f46ebd7714a4519ebdb453dbc20fa0ebdf
Nikita Prokopov <niki@tonsky.me>
2022-02-07 20:03:38 +0100

    JWM 0.32.2, Skija 0.98.1, Clojure 1.11.0-beta1 (closes #16)

--- script/common.py
+++ script/common.py
@@ -8,18 +8,18 @@ clojars = "https://repo.clojars.org"
 @functools.lru_cache(maxsize=1)
 def deps():
   deps = [
-    build_utils.fetch_maven("org.clojure", "clojure", "1.11.0-alpha3"),
+    build_utils.fetch_maven("org.clojure", "clojure", "1.11.0-beta1"),
     build_utils.fetch_maven("org.clojure", "core.specs.alpha", "0.2.62"),
-    build_utils.fetch_maven("org.clojure", "spec.alpha", "0.2.194"),
+    build_utils.fetch_maven("org.clojure", "spec.alpha", "0.3.218"),
     build_utils.fetch_maven("io.github.humbleui", "types", "0.1.2", classifier="clojure"),
     "src",
   ]
 
   parser = argparse.ArgumentParser()
   parser.add_argument('--jwm-dir', default=None)
-  parser.add_argument('--jwm-version', default="0.3.1")
+  parser.add_argument('--jwm-version', default="0.3.2")
   parser.add_argument('--skija-dir', default=None)
-  parser.add_argument('--skija-version', default='0.98.0')
+  parser.add_argument('--skija-version', default='0.98.1')
   (args, _) = parser.parse_known_args()
 
   if args.jwm_dir:
