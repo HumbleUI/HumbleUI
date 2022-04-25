bafc7e88b8767028368ac93005e64a2c47f40c07
Nikita Prokopov <niki@tonsky.me>
2022-04-25 16:50:27 +0200

    canvas.clj, draw hilber curve in canvas example

--- dev/examples/canvas.clj
+++ dev/examples/canvas.clj
@@ -1,5 +1,8 @@
 (ns examples.canvas
   (:require
+    [clojure.math :as math]
+    [io.github.humbleui.canvas :as canvas]
+    [io.github.humbleui.core :as core]
     [io.github.humbleui.paint :as paint]
     [io.github.humbleui.ui :as ui])
   (:import
@@ -7,6 +10,23 @@
     [io.github.humbleui.skija Canvas Paint]))
 
 (set! *warn-on-reflection* true)
+
+;; https://en.wikipedia.org/wiki/Hilbert_curve
+;; https://github.com/nextjournal/clerk-demo/blob/main/notebooks/logo.clj
+(defn hilbert-curve-impl
+  ([x-size y-size order]
+   (hilbert-curve-impl 0 0 x-size 0 0 y-size order))
+  ([x y xi xj yi yj n]
+   (if (<= n 0)
+     [[(+ x (/ (+ xi yi) 2)) (+ y (/ (+ xj yj) 2))]]
+     (concat
+       (hilbert-curve-impl x                       y                       (/ yi 2)     (/ yj 2)     (/ xi 2)     (/ xj 2)     (dec n))
+       (hilbert-curve-impl (+ x (/ xi 2))          (+ y (/ xj 2))          (/ xi 2)     (/ xj 2)     (/ yi 2)     (/ yj 2)     (dec n))
+       (hilbert-curve-impl (+ x (/ xi 2) (/ yi 2)) (+ y (/ xj 2) (/ yj 2)) (/ xi 2)     (/ xj 2)     (/ yi 2)     (/ yj 2)     (dec n))
+       (hilbert-curve-impl (+ x (/ xi 2) yi)       (+ y (/ xj 2) yj)       (- (/ yi 2)) (- (/ yj 2)) (- (/ xi 2)) (- (/ xj 2)) (dec n))))))
+
+(def hilbert-curve
+  (core/memoize-last hilbert-curve-impl))
 
 (def *last-event (atom nil))
 
@@ -17,17 +37,34 @@
 
 (defn on-paint [ctx ^Canvas canvas ^IPoint size]
   (let [{:keys [font-ui fill-text leading scale]} ctx
-        {:keys [width height]} size
-        event @*last-event]
-    (with-open [stroke (paint/stroke 0xFF000000 1)]
-      (.drawLine canvas 0 0 width height stroke)
-      (.drawLine canvas width 0 0 height stroke))
-    (loop [y  (+ 10 (* 2 leading scale))
-           kv (sort-by first event)]
-      (when-some [[k v] (first kv)]
-        (do
-          (.drawString canvas (str k " " v) (* 10 scale) y font-ui fill-text)
-          (recur (+ y (* 2 leading scale)) (next kv)))))))
+        {:keys [width height]} size]
+    
+    ;; bg
+    (let [size (max width height)]
+      (canvas/with-canvas canvas
+        (canvas/translate canvas (quot width 2) (quot height 2))
+        (canvas/rotate canvas 90)
+        (canvas/scale canvas 0.001)
+        (with-open [stroke (paint/stroke 0xFFCCCCCC (* scale 1000))]
+          (doseq [[[x1 y1] [x2 y2]] (partition 2 1 (hilbert-curve size size 6))
+                  :let [x1'  (- x1 (/ size 2))
+                        y1'  (- y1 (/ size 2))
+                        x2'  (- x2 (/ size 2))
+                        y2'  (- y2 (/ size 2))
+                        x1'' (- (* x1' x1') (* y1' y1'))
+                        y1'' (* 2 x1' y1')
+                        x2'' (- (* x2' x2') (* y2' y2'))
+                        y2'' (* 2 x2' y2')]]
+            (canvas/draw-line canvas x1'' y1'' x2'' y2'' stroke)))))
+
+    ;; event
+    (let [event @*last-event]
+      (loop [y  (+ 10 (* 2 leading scale))
+             kv (sort-by first event)]
+        (when-some [[k v] (first kv)]
+          (do
+            (canvas/draw-string canvas (str k " " v) (* 10 scale) y font-ui fill-text)
+            (recur (+ y (* 2 leading scale)) (next kv))))))))
 
 (def ui
   (ui/valign 0.5
---
+++ src/io/github/humbleui/canvas.clj
@@ -0,0 +1,56 @@
+(ns io.github.humbleui.canvas
+  (:import
+    [io.github.humbleui.types Rect IRect]
+    [io.github.humbleui.skija Canvas Font Paint]))
+
+(defn draw-line
+  ([^Canvas canvas p1 p2 ^Paint paint]
+   (.drawLine canvas (:x p1) (:y p1) (:x p2) (:y p2) paint))
+  ([^Canvas canvas x1 y1 x2 y2 ^Paint paint]
+   (.drawLine canvas x1 y1 x2 y2 paint)))
+
+(defn draw-rect [^Canvas canvas r ^Paint paint]
+  (condp instance? r
+    Rect (.drawRect canvas r paint)
+    IRect (.drawRect canvas (.toRect ^IRect r) paint)))
+
+(defn clear [^Canvas canvas color]
+  (.clear canvas (unchecked-int color)))
+
+(defn draw-string [^Canvas canvas s x y ^Font font ^Paint paint]
+  (.drawString canvas (str s) x y font paint))
+
+(defn translate [^Canvas canvas dx dy]
+  (.translate canvas dx dy))
+
+(defn scale
+  ([^Canvas canvas s]
+   (.scale canvas s s))
+  ([^Canvas canvas sx sy]
+   (.scale canvas sx sy)))
+
+(defn rotate [^Canvas canvas deg]
+  (.rotate canvas deg))
+
+(defn skew [^Canvas canvas sx sy]
+  (.skew canvas sx sy))
+
+(defn save [^Canvas canvas]
+  (.save canvas))
+
+(defn save-count [^Canvas canvas]
+  (.getSaveCount canvas))
+
+(defn restore
+  ([^Canvas canvas]
+   (.restore canvas))
+  ([^Canvas canvas count]
+   (.restoreToCount canvas count)))
+
+(defmacro with-canvas [canvas & body]
+  `(let [canvas# ~canvas
+         count# (.save canvas#)]
+     (try
+       ~@body
+       (finally
+         (.restoreToCount canvas# count#)))))
