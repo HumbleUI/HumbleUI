78b376d97beee85f23f93d3dc93119a61f2cbf36
Nikita Prokopov <niki@tonsky.me>
2023-12-30 19:23:57 +0100

    vdom: pretty-print nested #p

--- dev/user.clj
+++ dev/user.clj
@@ -42,21 +42,31 @@
       (throw res)
       res)))
 
-(def lock
+(def p-lock
   (Object.))
 
-(defn position []
+(defn p-pos []
   (let [trace (->> (Thread/currentThread)
                 (.getStackTrace)
                 (seq))
         el    ^StackTraceElement (nth trace 4)]
     (str "[" (clojure.lang.Compiler/demunge (.getClassName el)) " " (.getFileName el) ":" (.getLineNumber el) "]")))
 
+(defn p-impl [position form res]
+  (let [form (clojure.walk/postwalk
+               (fn [form]
+                 (if (and
+                       (list? form)
+                       (= 'user/p-impl (first form)))
+                   (clojure.lang.TaggedLiteral/create 'p (nth form 3))
+                   form))
+               form)]
+    (locking p-lock
+      (println (str position " #p " form " => " (pr-str res))))
+    res))
+
 (defn p [form]
-  `(let [res# ~form]
-     (locking lock
-       (println (str "#p" (position) " " '~form " => " (pr-str res#))))
-     res#))
+  `(p-impl (p-pos) '~form ~form))
 
 (defn set-floating! [window floating]
   (when window
