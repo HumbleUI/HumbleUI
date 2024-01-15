8281ab88ba14b025c83af79f74ebc3c3e91af32c
Nikita Prokopov <niki@tonsky.me>
2024-01-15 02:12:50 +0100

    Use image filtering by default

--- src/io/github/humbleui/ui/image.clj
+++ src/io/github/humbleui/ui/image.clj
@@ -3,9 +3,9 @@
     [io.github.humbleui.core :as core]
     [io.github.humbleui.protocols :as protocols])
   (:import
-    [io.github.humbleui.skija Canvas Image]))
+    [io.github.humbleui.skija Canvas Image SamplingMode]))
 
-(core/deftype+ AnImage [^Image image]
+(core/deftype+ AnImage [^Image image opts]
   :extends core/ATerminal
   protocols/IComponent
   (-measure [_ _ctx cs]
@@ -13,10 +13,22 @@
       (core/ipoint (:width cs) (/ (:width cs) aspect))))
   
   (-draw [_ _ctx rect ^Canvas canvas]
-    (let [aspect (/ (.getWidth image) (.getHeight image))
-          rect'  (core/rect-xywh (:x rect) (:y rect) (:width rect) (/ (:width rect) aspect))]
-      (.drawImageRect canvas image rect'))))
+    (let [aspect   (/ (.getWidth image) (.getHeight image))
+          src-rect (core/rect-xywh 0 0 (.getWidth image) (.getHeight image))
+          dst-rect (core/rect-xywh (:x rect) (:y rect) (:width rect) (/ (:width rect) aspect))
+          sampling (case (:sampling-mode opts)
+                     :nearest     SamplingMode/DEFAULT
+                     :linear      SamplingMode/LINEAR
+                     :mitchell    SamplingMode/MITCHELL
+                     :catmull-rom SamplingMode/CATMULL_ROM
+                     nil          SamplingMode/MITCHELL
+                     (:sampling-mode opts))]
+      (.drawImageRect canvas image src-rect dst-rect sampling #_:paint nil #_:strict false))))
 
-(defn image [src]
-  (let [image (Image/makeFromEncoded (core/slurp-bytes src))]
-    (map->AnImage {:image image})))
+(defn image
+  ([src]
+   (image {} src))
+  ([opts src]
+   (let [image (Image/makeFromEncoded (core/slurp-bytes src))]
+     (map->AnImage {:image image
+                    :opts opts}))))
