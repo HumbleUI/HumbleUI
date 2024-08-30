0c4006e861def3c722d347acd8b382e78c43092d
Victor Vollbrecht <victorvollbrecht@outlook.com>
2024-08-30 22:24:11 +0200

    Add nix flake

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "HumbleUI - Clojure Desktop UI framework";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/release-24.05";
+  inputs.flake-parts.url = "github:hercules-ci/flake-parts";
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+      perSystem =
+        { pkgs, ... }:
+        {
+          devShells.default = pkgs.mkShell {
+            nativeBuildInputs = with pkgs; [
+              jdk22
+              python3
+            ];
+            LD_LIBRARY_PATH = "${pkgs.libGL}/lib/:$LD_LIBRARY_PATH";
+            shellHook = ''
+              ./script/repl.py
+            '';
+          };
+        };
+    };
+}
